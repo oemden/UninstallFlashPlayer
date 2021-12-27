ee76f750c009607a16088c44577e7dc38b6264de
Olivier Emsellem <oemden@users.noreply.github.com>
2021-12-27 17:50:27 +0100

    adding a quit Adobe Flash Player Install Manager.app
    Quitting the FlashPlayer app if running

--- UninstallFlashPlayer.sh
+++ UninstallFlashPlayer.sh
@@ -4,7 +4,11 @@
 ##
 Version="0.1.1"
 ## Source : https://soundmacguy.wordpress.com/2020/01/24/uninstalling-adobe-flash-player-in-a-flash/
-
+## adding a quit Adobe Flash Player Install Manager.app
 clear ; echo
 
+osascript -e 'quit app "Adobe Flash Player Install Manager"' # put this as a precheck_script in munki
+
 "/Applications/Utilities/Adobe Flash Player Install Manager.app/Contents/MacOS/Adobe Flash Player Install Manager" -uninstall
+
+
